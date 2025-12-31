584557b2535cb323e6888f368e0eca1a9747a8df
developerarhan <khanarhan0205@gmail.com>
2025-12-31 18:02:29 +0530

    Improved API error handling

--- api/views.py
+++ api/views.py
@@ -2,18 +2,28 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import StudyPlanSerializer
 from .ai_utils import generate_study_plan
+from rest_framework import status
 
 # Create your views here.
 
 @api_view(['POST'])
 def study_plan(request):
     serializer = StudyPlanSerializer(data=request.data)
-    if serializer.is_valid():
-        data = serializer.validated_data
-        plan = generate_study_plan(
-            data['subject'],
-            data['hours_per_day'],
-            data['days']
+
+    if not serializer.is_valid():
+        return Response(
+            {"error": "Invalid input", "details": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
         )
-        return Response({"plan": plan})
-    return Response(serializer.errors, status=400)
+
+    data = serializer.validated_data
+    plan = generate_study_plan(
+        data['subject'],
+        data['hours_per_day'],
+        data['days']
+    )
+
+    return Response(
+        {"plan": plan},
+        status=status.HTTP_200_OK
+    )
