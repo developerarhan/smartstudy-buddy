297b59654b2cebcb3e2c6635cb2f09588cff57ca
developerarhan <khanarhan0205@gmail.com>
2026-01-02 16:33:10 +0530

    Fix Render 400 error (allowed hosts + csrf)

--- smartstudy/settings.py
+++ smartstudy/settings.py
@@ -31,11 +31,16 @@ SECRET_KEY = 'django-insecure-f0c6mhzs-bo%q*biwy&^%%(71v&6b@8o!b^(_q$(j(kx3q#5z^
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "smartstudy-backend.onrender.com",
+    "smartstudy-backend-1okw.onrender.com",
     "localhost",
     "127.0.0.1"
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://smartstudy-backend-1okw.onrender.com"
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Application definition
 
